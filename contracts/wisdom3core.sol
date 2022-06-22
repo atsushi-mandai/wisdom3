5376c409101218885d022431d43156b8238e27ae
"atsushi-mandai" <"atsushi.mandai.1991@gmail.com">
2022-06-22 14:56:30 +0900

    added basicFee and function changeBasicFee

--- contracts/wisdom3core.sol
+++ contracts/wisdom3core.sol
@@ -12,6 +12,12 @@ import "./access/Ownable.sol";
 contract Wisdom3Core is Wisdom3Token, Ownable {
 
     event AnnotationCreated(uint annotationId, string url, string body, string languageCode);
+
+    /**
+    * @dev basicFee is used to determine the amount of WSDM paid
+    * by the reader who wants to read the annotations.
+    */
+    uint basicFee = 1;
     
     /**
     * @dev "annotation" is the basic structure of Wisdom3.
@@ -47,4 +53,14 @@ contract Wisdom3Core is Wisdom3Token, Ownable {
         uint annotationId = annotations.length - 1;
         emit AnnotationCreated(annotationId, _url, _body, _languageCode);
     }
+
+    /**
+    * @dev changeBasicFee function lets contract owner to change the basicFee.
+    * Owner of the contract will be transfered to the community in the future.
+    */
+    function changeBasicFee(uint _newBasicFee) public onlyOwner {
+        basicFee = _newBasicFee;
+    }  
+
+    
 }
