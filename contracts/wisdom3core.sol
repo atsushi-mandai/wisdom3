c3a88dc2d848e2d5c5ef2cacf1f732f905167915
"atsushi-mandai" <"atsushi.mandai.1991@gmail.com">
2022-06-24 16:38:04 +0900

    imported SafeMath,  added_stakeToAnnotation function

--- contracts/wisdom3core.sol
+++ contracts/wisdom3core.sol
@@ -5,11 +5,14 @@ pragma solidity ^0.8.0;
 
 import "./token/ERC20/Wisdom3Token.sol";
 import "./access/Ownable.sol";
+import "./utils/math/SafeMath.sol";
 
 /// @title Wisdom3core
 /// @author Atsushi Mandai
 /// @notice Basic functions of the Wisdome3 will be written here.
 contract Wisdom3Core is Wisdom3Token, Ownable {
+
+    using SafeMath for uint256;
 
     /**
     *
@@ -33,8 +36,18 @@ contract Wisdom3Core is Wisdom3Token, Ownable {
     /**
     * @dev basicFee is used to determine the amount of WSDM paid
     * by the reader who wants to read the annotations.
+    * basicFee should be determined by a vote of governance token holders in the future
     */
     uint basicFee = 1;
+
+    /**
+    * @dev When the curator stakes his/her WSDM to an annotation,
+    * it cannot be pulled out until the minimumStakePeriod has elapsed. 
+    * This prevents malicious front-end providers from proactively staking to an annotation 
+    * just before it is purchased by the reader.
+    * basicFee should be determined by a vote of governance token holders in the future
+    */
+    uint32 minimumStakePeriod = 30 days;
     
     /**
     * @dev "annotation" is the basic structure of Wisdom3.
@@ -56,12 +69,14 @@ contract Wisdom3Core is Wisdom3Token, Ownable {
     /**
     * @dev Each annotation could be staked with WSDM.
     * Annotation with more stakes of WSDM are considered more valuable annotation.
-    * Therefore, it will be displayed preferentially, and the author & curator of it will receive more rewards.
+    * Therefore, it will be displayed preferentially, 
+    * and the author & curator of it will receive more rewards.
     */
     struct annotationStake {
         uint annotationId;
-        address curatorAddress;
         uint stakeAmount;
+        address curatorAddress;
+        uint32 withdrawAllowTime;
     }
     annotationStake[] public annotationStakes;
 
@@ -76,6 +91,7 @@ contract Wisdom3Core is Wisdom3Token, Ownable {
 
     /**
     * @dev changeBasicFee function lets contract owner to change the basicFee.
+    * changeMiniumStakePeriod function lets contract owner to change the minimumStakePeriod.
     * Owner of the contract will be transfered to the community in the future.
     * If few readers pay WSDM to read the annotations, the basic fee should be reduced to stimulate demand.
     * Conversely, if many readers pay WSDM to read the annotations, the community could conversely raise the 
@@ -84,6 +100,10 @@ contract Wisdom3Core is Wisdom3Token, Ownable {
     function changeBasicFee(uint _newBasicFee) public onlyOwner {
         basicFee = _newBasicFee;
     }  
+
+    function changeMinimumStakePeriod(uint32 _newMinimumStakePeriod) public onlyOwner {
+        minimumStakePeriod = _newMinimumStakePeriod;
+    }
 
     /**
     * @dev "createAnnotation" lets anyone to create an annotation.
@@ -110,9 +130,14 @@ contract Wisdom3Core is Wisdom3Token, Ownable {
     *
     */
 
-    function _stakeToAnnotation(uint _id, uint _ammount) internal {
-
+    /**
+    * @dev _stakeToAnnotation is an internal function to be called
+    * after the transaction has been checked from several perspectives.
+    * !!! better use SafeMath in this function but it doesn't work somehow.
+    */
+    function _stakeToAnnotation(uint _annotationId, uint _ammount) internal {
+        annotationStakes.push(annotationStake(_annotationId, _ammount, _msgSender(), uint32(block.timestamp) + minimumStakePeriod));
+        annotations[_annotationId].totalStake = annotations[_annotationId].totalStake + _ammount;
     }
 
-    
 }
